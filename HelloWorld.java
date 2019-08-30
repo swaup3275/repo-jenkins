c74f825bbe94364aa5a1c8bfd460045a207d6f8b
swaup3275 <swarup32756@gmail.com>
2019-08-30 15:27:32 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        Thread.sleep(2000);
+        System.out.println("Hello, World updated");
+    }
+
+}
