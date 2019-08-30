0235d04bb4b944c232731766a7823518d1ab7687
swaup3275 <swarup32756@gmail.com>
2019-08-30 15:28:54 +0530

    Update file1.java

--- file1.java
+++ file1.java
@@ -3,7 +3,8 @@ public class file1 {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        Thread.sleep(2000);
+        System.out.println("Hello, World updated");
     }
 
 }
